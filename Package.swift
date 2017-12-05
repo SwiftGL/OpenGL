70b765437206a2b8b45c373bb70abbeb61004325
Justin Kolb <franticapparatus@gmail.com>
2017-12-05 07:20:11 -0600

    Renamed Tools target to glgen.

--- Package.swift
+++ Package.swift
@@ -6,12 +6,12 @@ let package = Package(
     name: "SGLOpenGL",
     products: [
         .library(name: "SGLOpenGL", targets: ["SGLOpenGL"]),
-        .executable(name: "glgen", targets: ["Tools"]),
+        .executable(name: "glgen", targets: ["glgen"]),
     ],
     dependencies: [
     ],
     targets: [
         .target(name: "SGLOpenGL", dependencies: []),
-        .target(name: "Tools", dependencies: []),
+        .target(name: "glgen", dependencies: []),
     ]
 )
